1dd5cccfb9cff681570c660fb131e9e8809182ce
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-28 17:21:44 -0300

    Categoria initiated

---
+++ src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom';
+
+export default function CadastroCategoria(){
+    return(
+        <PageDefault>
+            <h1>Cadastro de categoria</h1>
+
+            <Link to="/">
+                Ir para Home
+            </Link>
+        </PageDefault>
+    );
+}
