f7af5993afa2d8815b152895eb7d6cdc273425ea
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-28 17:57:02 -0300

    Categoria updated

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,19 @@ export default function CadastroCategoria(){
         <PageDefault>
             <h1>Cadastro de categoria</h1>
 
+            <form>
+                <label>
+                    Nome da categoria:
+                    <input
+                        type="text"
+                    />
+
+                    <button>
+                        Cadastrar
+                    </button>
+                </label>
+            </form>
+
             <Link to="/">
                 Ir para Home
             </Link>
