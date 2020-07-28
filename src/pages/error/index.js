363c54d9bb19a39c3e0bcf4337eb7e171c9727e7
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-28 17:22:12 -0300

    Error initiated

---
+++ src/pages/error/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Pagina404(){
+    return(
+        <div>
+            <a href="https://editor.p5js.org/juuliana/full/aMT8gFp9F">Entre na Aventura</a>
+        </div>
+    );
+}
