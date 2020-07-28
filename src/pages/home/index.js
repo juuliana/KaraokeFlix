19ff03a6143c9a9d783e3da8eba8be15860dca16
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-28 17:23:02 -0300

    home updated

---
+++ src/pages/home/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
+
+function Home() {
+  return (
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Bem-vindo ao SINGFLIX - Uma plataforma incrível para se divertir com os amigos! Está esperando o que para escolher sua música e soltar a voz com o nosso KAROAKÊ"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
