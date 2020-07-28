c08382928557b97a743f7b1e7b31e79dad470d70
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-28 17:22:52 -0300

    src updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+
+import Pagina404 from './pages/error';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route exact path="/cadastro/video"component={CadastroVideo}/>
+      <Route exact path="/cadastro/categoria"component={CadastroCategoria}/>
+      <Route component={Pagina404}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
