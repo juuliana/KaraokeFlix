2ca1f4d253cea92f9292ccc4b0126baad61a8541
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-29 00:11:28 -0300

    error game -add

--- src/pages/error/index.js
+++ src/pages/error/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PageDefault from '../../components/PageDefault';
 
 export default function Pagina404(){
     return(
-        <div>
-            <a href="https://editor.p5js.org/juuliana/full/aMT8gFp9F">Entre na Aventura</a>
-        </div>
+       
+       <PageDefault>
+            <h2 align="center">Não encontramos o que está procurando :( - enquanto isso o que acha de entrar na Aventura com a Hipsta?</h2>
+        
+        <iframe
+            title="AngryBirds"
+            src="https://editor.p5js.org/juuliana/full/aMT8gFp9F"
+            width="100%"
+            height="700px"
+            scrolling="no"
+            border="0"
+        ></iframe>
+       </PageDefault>
+        
+
     );
 }
