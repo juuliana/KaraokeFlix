e467a555ce4020c8a92f6006f469be008cb7e7dc
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-30 18:52:27 -0300

    Footer updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FooterBase } from './styles';
 
+import Logo from '../../assets/img/julianaribeiro.png';
+
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://github.com/juuliana">
+        <img className="Logo" src={Logo} alt="LogoJuliana" />
       </a>
       <p>
         Orgulhosamente criado durante a
