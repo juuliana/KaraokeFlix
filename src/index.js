92ff75d1b3e93fb7adbe87b651afc1fdb277a060
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-29 23:28:14 -0300

    karoake -add

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import CadastroVideo from './pages/cadastro/Video';
 
 import Pagina404 from './pages/error';
 import CadastroCategoria from './pages/cadastro/Categoria';
+import Karoake from './components/Karoake';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -15,6 +16,7 @@ ReactDOM.render(
       <Route exact path="/" component={App}/>
       <Route exact path="/cadastro/video"component={CadastroVideo}/>
       <Route exact path="/cadastro/categoria"component={CadastroCategoria}/>
+      <Route exact path="/karoake"component={Karoake}/>
       <Route component={Pagina404}/>
     </Switch>
   </BrowserRouter>,
