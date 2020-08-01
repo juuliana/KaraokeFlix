d0d13d4d9e121ecd59ff9d574653db463873d6bb
Juliana Ribeiro <jumaju10@hotmail.com>
2020-08-01 15:49:20 -0300

    cadastroCategoria updated

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,33 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
-
 import Button from '../../../components/Button';
+import useForm from '../../../hooks/useForm';
 
-export default function CadastroCategoria() {
-  const valoresIniciais = {
-    name: '',
-    description: '',  
-    color: '',
+import categoriasRepository from '../../../repositories/categorias';
+import {TableC} from '../styles';
+
+function CadastroCategoria() {
+  const history = useHistory();
+
+  const table = {
+    borderCollapse: 'collapse',
+    width: '70%',
+    marginTop: '20px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
   };
 
+  const tableth = {
+    paddingTop: '12px',
+    paddingBottom: '12px',
+    textAlign: 'center',
+    backgroundColor: '#fbd46d',
+    color: 'black',
+
+  };
+
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+
+  const { handleChange, values, clearForm } = useForm(valoresIniciais);
+
   const [categorias, setCategorias] = useState([]);
-  const [values, setValues] = useState(valoresIniciais);
-
-  function setValue(key, value) {
-    setValues({
-      ...values,
-      [key]: value,
-    });
-  }
-
-  function handleChange(infosDoEvento) {
-    setValue(
-      infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value,
-    );
-  }
 
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
@@ -35,97 +45,85 @@ export default function CadastroCategoria() {
       : 'https://karoakeflix.herokuapp.com/categorias';
 
     fetch(URL_TOP)
-      .then(async(respostaDoServidor)=> {
+      .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
         setCategorias([
           ...resposta,
         ]);
       });
-
-    // setTimeout(() => {
-    //   setCategorias([
-    //     ...categorias,
-    //     {
-    //       "id": 1,
-    //       "name": "Internacional",
-    //       "description": "Músicas Intercionais!",
-    //       "cor": "#c81912"
-    //     },
-    //     {
-    //       "id": 2,
-    //       "name": "Sertanejo Universitário",
-    //       "description": "Músicas de Sertanejo Universitário!",
-    //       "cor": "#cbd1ff"
-    //     },
-    //     {
-    //       "id": 3,
-    //       "name": "Sertanejo Raiz",
-    //       "description": "Músicas de Sertanejo Raiz!",
-    //       "cor": "#ffa41b"
-    //     },
-    //     {
-    //       "id": 4,
-    //       "name": "Pagode e Samba",
-    //       "description": "Músicas de Pagode e Samba!",
-    //       "cor": "#00a8cc"
-    //     },
-    //     {
-    //       "id": 5,
-    //       "name": "Pop",
-    //       "description": "Músicas Pop!",
-    //       "cor": "#000839"
-    //     },
-    //     {
-    //       "id": 6,
-    //       "name": "Religiosa",
-    //       "description": "Músicas Religiosas!",
-    //       "cor": "#004445"
-    //     }
-    //   ]);
-    // }, 4 * 1000);
-
   }, []);
 
+  const listC = (
+    <table style={table}>
+      <tbody>
+        <tr>
+          <th style={tableth}>Categorias</th>
+        </tr>
+      </tbody>
+      {categorias.map((categoria) => (
+        <tbody key={`${categoria.titulo}`}>
+          <TableC fieldColor={categoria.cor}>
+            <td style={{ border: `2px solid white`, padding: '8px', textAlign: 'center' }}>
+              {categoria.titulo}
+            </td>
+          </TableC>
+        </tbody>
+      ))}
+    </table>
+  );
+
+
+  
   return (
+    
     <PageDefault>
       <h1>
-        Cadastro de categoria:
-        {values.name}
+        Cadastro de Categoria:
+        {values.titulo}
       </h1>
 
       <form onSubmit={function handleSubmit(infosDoEvento) {
         infosDoEvento.preventDefault();
-
         setCategorias([
           ...categorias,
           values,
         ]);
 
-        setValues(valoresIniciais);
+        categoriasRepository.create({
+           titulo: values.titulo,
+           descricao: values.descricao,
+           cor: values.cor,
+         })
+           .then(() => {
+             console.log('Cadastrou com sucesso!');
+             history.push('/');
+           });
+        
+        clearForm();
       }}
       >
 
         <FormField
-          label="Nome da Categoria:"
+          label="Título da Categoria"
           type="text"
-          name="name"
-          value={values.name}
+          name="titulo"
+          value={values.titulo}
           onChange={handleChange}
         />
 
         <FormField
-          label="Descrição:"
+          label="Descrição"
           type="textarea"
-          name="description"
-          value={values.description}
+          name="descricao"
+          value={values.descricao}
           onChange={handleChange}
         />
 
         <FormField
-          label="Cor:"
+          label="Cor"
           type="color"
-          name="color"
-          value={values.color}
+          name="cor"
+          value={values.cor}
           onChange={handleChange}
         />
 
@@ -136,21 +134,15 @@ export default function CadastroCategoria() {
 
       {categorias.length === 0 && (
         <div>
+          {/* Cargando... */}
           Loading...
         </div>
       )}
 
-      <ul>
-        {categorias.map((categoria, indice) => (
-          <li key={`${categoria.name}${indice}`}>
-            {categoria.name}
-          </li>
-        ))}
-      </ul>
+      {listC}
 
-      <Link to="/">
-        Ir para Home
-      </Link>
     </PageDefault>
   );
 }
+
+export default CadastroCategoria;
