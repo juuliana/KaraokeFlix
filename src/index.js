6f1e6d5c61f2d474a122008b166face657f37b55
Juliana Ribeiro <jumaju10@hotmail.com>
2020-08-01 15:47:44 -0300

    index updated

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/home';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import CadastroVideo from './pages/cadastro/Video';
+import Home from './pages/home';
 
 import Pagina404 from './pages/error';
 import CadastroCategoria from './pages/cadastro/Categoria';
-import Karoake from './components/Karoake';
+import CadastroVideo from './pages/cadastro/Video';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={App}/>
+      <Route exact path="/" component={Home}/>
       <Route exact path="/cadastro/video"component={CadastroVideo}/>
       <Route exact path="/cadastro/categoria"component={CadastroCategoria}/>
-      <Route exact path="/karoake"component={Karoake}/>
       <Route component={Pagina404}/>
     </Switch>
   </BrowserRouter>,
