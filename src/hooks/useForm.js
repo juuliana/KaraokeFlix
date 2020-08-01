3e20b816976660d9915d2d17eebfff9b953fb7ea
Juliana Ribeiro <jumaju10@hotmail.com>
2020-08-01 15:48:49 -0300

    useForm updated

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,29 +1,31 @@
 import { useState } from 'react';
 
-export default function useForm(valoresIniciais){
-    const [values, setValues] = useState(valoresIniciais);
-  
-    function setValue(key, value) {
-      setValues({
-        ...values,
-        [key]: value,
-      });
-    }
-  
-    function handleChange(infosDoEvento) {
-      setValue(
-        infosDoEvento.target.getAttribute('name'),
-        infosDoEvento.target.value,
-      );
-    }
-  
-    function clearForm(){
-      setValues(valoresIniciais);
-    }
-  
-    return{
-      values,
-      handleChange,
-      clearForm
-    }
+function useForm(valoresIniciais) {
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
+    );
+  }
+
+  function clearForm() {
+    setValues(valoresIniciais);
+  }
+
+  return {
+    values,
+    handleChange,
+    clearForm,
+  };
 }
+
+export default useForm;
