9b75861d6aa145f9ade77c6b74ce4a8d65af4bc8
Juliana Ribeiro <jumaju10@hotmail.com>
2020-08-01 15:49:02 -0300

    styles cadastro -add

---
+++ src/pages/cadastro/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom'
+
+export const StyledLink = styled(Link)`
+color: var(--white);
+    border: 1px solid var(--white);
+    background: var(--black);
+    box-sizing: border-box;
+    cursor: pointer;
+    padding: 16px 24px;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    outline: none;
+    border-radius: 5px;
+    text-decoration: none;
+    display: inline-block;
+    transition: opacity .3s;
+
+    &:hover,
+    &:focus {
+        opacity: .5;
+    }
+`
+
+export const ButtonWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+export const TableC = styled.tr`
+    :hover{
+        background-color: ${(props) => props.fieldColor || 'black'};
+    } 
+`;
