f629a46ca98ef80d720bd499b9d3f57a62acef5b
Juliana Ribeiro <jumaju10@hotmail.com>
2020-08-01 22:55:46 -0300

    hipsta

--- src/pages/error/index.js
+++ src/pages/error/index.js
@@ -4,13 +4,12 @@ import PageDefault from '../../components/PageDefault';
 export default function Pagina404(){
     return(
        <PageDefault>
-            <h2 align="center">Não encontramos o que está procurando :( - enquanto isso o que acha de entrar na Aventura com a Hipsta?</h2>
-        
+            <h3 align="center">Não encontramos o que está procurando :( enquanto isso entre na Aventura com a Hipsta</h3>
         <iframe
             title="AngryBirds"
             src="https://editor.p5js.org/juuliana/full/aMT8gFp9F"
             width="100%"
-            height="700px"
+            height="450px"
             scrolling="no"
             border="0"
         ></iframe>
