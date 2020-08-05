e67ace9ef106a9b6dc04857c94747033c2de45b9
Juliana Ribeiro <jumaju10@hotmail.com>
2020-08-05 00:16:35 -0300

    footer updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ import Logo from '../../assets/img/julianaribeiro.png';
 function Footer() {
   return (
     <FooterBase className="FooterFx">
-      <a href="https://github.com/juuliana">
+      <a href="https://www.linkedin.com/in/juliana-dos-santos-ribeiro-b721b6197/">
         <img className="Logo" src={Logo} alt="LogoJuliana" />
       </a>
     </FooterBase>
